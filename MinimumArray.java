aad3aaacc5e71d6cbf3613b5a5fbab051004dae0
anisaifah <anisaifah@gmail.com>
2016-01-27 14:06:07 +0700

    Update MinimumArray.java

--- MinimumArray.java
+++ MinimumArray.java
@@ -2,15 +2,15 @@ public class MinimumArray{
 
   public int checkMinimum(int[] A, int[] B){
     // write your solution here
-    int minim = 999, x, y = 0, bdg;
+	int minim = 999, x, y = 0, n;
 		for(int i=0; i<A.length; i++){
 			x = A[i];
 			for (int j=0; j<B.length; j++){
 				y = B[j];
-				if (x = y){
-					bdg = y;
-					if (bdg<minim){
-						minim = bdg;
+				if (x == y){
+					n = y;
+					if (n<minim){
+						minim = n;
 					}
 				}
 			}
@@ -23,13 +23,13 @@ public class MinimumArray{
 		}
 	}
     
-  public static void main(String[] args){
-    MimimumArray m = new MinimumArray();
+		public static void main(String[] args){
+		MinimumArray m = new MinimumArray();
     
-    int[] A = { 1, 3, 2, 1 };
+			int[] A = { 1, 3, 2, 1 };
     int[] B = { 4, 2, 5, 3, 2 };
     
     int result = m.checkMinimum( A, B );
-    
+    System.out.println("Hasil dari fungsi minimum array adalah "+result);
   }
 }
