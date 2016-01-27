d33ab95a5e8f9643c69c1a8b022dba6eaaf80320
anisaifah <anisaifah@gmail.com>
2016-01-27 17:40:25 +0700

    Update OneSwapSort.java

--- OneSwapSort.java
+++ OneSwapSort.java
@@ -1,20 +1,46 @@
+// 1103130239, 1103132235, 1103134467, IF-38-01
+
+import java.util.Arrays;
 public class OneSwapSort{
   
   public int checkSort( int[] A ){
-    // write your solution here
+	// write your solution here
+	return 0;
   }  
   
   public int checkSwap(int[] A){
-    // write your solution here
+	int a = A[0];
+	int b = 0;
+	
+	for(int i = 0 ; i < A.length ; i++){
+		if(a > A[i]){
+			b++;
+		}
+		a = A[i];
+	}
+	if(b > 1){
+		return 0;
+	}
+	else {
+		return 1;
+	}
   }
   
-  public static void main(String[] args){
-    OneSwapSort swap = new OneSwapSort();
+	public static void main(String[] args){
+		OneSwapSort sort = new OneSwapSort();
+		OneSwapSort swap = new OneSwapSort();
+		int[] A = {1, 5, 3, 3, 7} ;
     
-    int[] A = { 1, 5, 3, 3, 7 } ;
-    
-    int result = swap.checkSwap( A );
-    
-  }
-  
+		Arrays.sort(A);
+		System.out.println("sort : ");
+		for(int i: A){
+			
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		System.out.println();
+		int result = swap.checkSwap(A);
+		System.out.println("swap : ");
+		System.out.println(result + " ");
+    }
 }
